28a3ec21d18770b0fc9de453719775c0c67ee2aa
Evan Pierce <47014572+EvanPierce97@users.noreply.github.com>
2019-02-06 12:16:32 -0500

    Final Build

--- Assets/Scenes/Scripts/ApplePicker.cs
+++ Assets/Scenes/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -8,14 +9,45 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
-    void Start()
+    public List<GameObject> basketList;
+
+      void Start()
     {
-        for (int i = 0; i < numBaskets; i++)
+        basketList = new List<GameObject>();
+
+         for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
-        }
+            basketList.Add(tBasketGO);                                    // 3
+
+       }
     }
-}
+
+    public void AppleDestroyed()
+    {                                          // 2
+        // Destroy all of the falling Apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");// 3
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+        //// Destroy one of the Baskets
+        // Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count - 1;
+        // Get a reference to that Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+        // Remove the Basket from the List and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+        // Restart the game, which doesn't affect HighScore.Score
+        if (basketList.Count == 0)
+        {
+            Application.LoadLevel("_Scene_0");
+        }
+
+   }
+
+ }
--- Assets/Scenes/Scripts/Basket.cs
+++ Assets/Scenes/Scripts/Basket.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    public Text scoreGT;                                       // 1
 
-    // Update is called once per frame
-    void Update()
+    void Start()
+    {
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");               // 2
+        // Get the GUIText Component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();                          // 3
+        // Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
+      void Update()
     {
         // Get the current screen position of the mouse from Input
         Vector3 mousePos2D = Input.mousePosition;                           // 1
@@ -28,5 +38,18 @@ public class Basket : MonoBehaviour
             Destroy(collidedWith);
         }
 
-    }
+        // Parse the text of the scoreGT into an int
+        int score = int.Parse(scoreGT.text);                              // 4
+        // Add points for catching the apple
+        score += 100;
+        // Convert the score back to a string and display it
+        scoreGT.text = score.ToString();
+
+        // Track the high score
+        if (score > HighScore.score)
+        {
+            HighScore.score = score;
+        }
+
+     }
 }

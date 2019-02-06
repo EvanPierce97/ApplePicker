bff4af09ee76f7ee7a1eb6d7a6366a0f6007acda
Evan Pierce <47014572+EvanPierce97@users.noreply.github.com>
2019-02-06 11:21:47 -0500

    Added second scene

--- Assets/Scenes/Scripts/Apple.cs
+++ Assets/Scenes/Scripts/Apple.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using System.Collections;
 public class Apple : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public static float bottomY = -20f;
     void Update()
     {
-        
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
